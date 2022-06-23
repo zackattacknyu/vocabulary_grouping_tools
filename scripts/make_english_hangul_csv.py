93f33e87f70b3e07f12db58510db3438d44daf67
Zach DeStefano <zach.destefano@hinge.co>
2022-06-23 00:55:26 -0400

    Add scripts to make CSV of hangul, english pairs

---
+++ scripts/make_english_hangul_csv.py
@@ -0,0 +1,18 @@
+import os
+
+from vocabulary_grouping_tools.ingestion.english_korean_info_ingest import text_block_to_english_hangul_df
+
+
+FILE_NAME = 'vocab_active_korean_1_2_ttmik_1_2'
+EXTENSION = 'txt'
+FILE_PATH = 'input_external_files'
+OUTPUT_PATH = f'output/{FILE_NAME}'
+
+with open(f'{FILE_PATH}/{FILE_NAME}.{EXTENSION}', 'r') as f:
+    block = f.read()
+
+
+eng_hangul_df = text_block_to_english_hangul_df(block)
+
+os.makedirs(OUTPUT_PATH, exist_ok=True)
+eng_hangul_df.to_csv(f'output/{FILE_NAME}/{FILE_NAME}_table.csv', index=False)
---
+++ src/vocabulary_grouping_tools/util/constants.py
@@ -0,0 +1,2 @@
+HANGUL_COL_NM = 'hangul_word'
+ENGLISH_COL_NM = 'english phrase'
